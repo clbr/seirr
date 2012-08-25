36703f7df55675a6ca42435a3519cd7c5fb26f52
Lauri Kasanen <f6133@jamk.fi>
2012-08-25 21:14:52 +0300

    Remove irr loading

--- include/IrrCompileConfig.h
+++ include/IrrCompileConfig.h
@@ -257,7 +257,7 @@ B3D, MS3D or X meshes */
 #endif
 
 //! Define _IRR_COMPILE_WITH_IRR_MESH_LOADER_ if you want to load Irrlicht Engine .irrmesh files
-#define _IRR_COMPILE_WITH_IRR_MESH_LOADER_
+//#define _IRR_COMPILE_WITH_IRR_MESH_LOADER_
 
 //! Define _IRR_COMPILE_WITH_MD2_LOADER_ if you want to load Quake 2 animated files
 #define _IRR_COMPILE_WITH_MD2_LOADER_
